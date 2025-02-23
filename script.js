e94791ef7b329d5cd9cde2fb3c723b5a7605c08e
Taraka Kiranmayi <tarakakiranmayi@gamil.com>
2025-02-23 22:24:22 +0530

    so then it is final commit

--- script.js
+++ script.js
@@ -1,31 +1,4 @@
-/* Toggle Style Switcher */
 
-const styleSwitcherToggle = document.querySelector('.style-switcher-toggler');
-styleSwitcherToggle.addEventListener('click', () => { document.querySelector('.style-switcher').classList.toggle('open'); })
-
-window.addEventListener('scroll', () => { if(document.querySelector('.style-switcher').classList.contains('open')) { document.querySelector('.style-switcher').classList.remove('open'); } })
-
-const alternateStyles = document.querySelectorAll('.alternate-style');
-function setActiveStyle(color) {
-    alternateStyles.forEach((style) => {
-        if(color === style.getAttribute('title')) { style.removeAttribute('disabled'); } else { style.setAttribute('disabled', 'true'); }
-    })
-}
-
-/* Dark/Light Mode */
-
-const dayNight = document.querySelector('.day-night');
-dayNight.addEventListener('click', () => {
-    dayNight.querySelector('i').classList.toggle('fa-sun');
-    dayNight.querySelector('i').classList.toggle('fa-moon');
-    document.body.classList.toggle('dark');
-})
-
-window.addEventListener('load', () => {
-    if(document.body.classList.contains('dark')) { dayNight.querySelector('i').classList.add('fa-sun'); } else { dayNight.querySelector('i').classList.add('fa-moon'); }
-})
-
-/* Typing Animation */
 
 var typed = new Typed('.typing', { strings: ["", "AI Engineer", "Web Developer",""], typeSpeed: 100, Backspeed: 60, loop: true })
 
