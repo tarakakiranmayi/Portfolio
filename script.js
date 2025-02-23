6b168387d212d37fb3ed02d739993f60b1f0a4a5
Taraka Kiranmayi <tarakakiranmayi@gamil.com>
2025-02-23 21:59:09 +0530

    made new version of porfolio

--- script.js
+++ script.js
@@ -1,7 +1,91 @@
-document.getElementById('toggle-btn').addEventListener('click', function() {
-    const navLinks = document.getElementById('nav-links');
-    const body = document.body; // Reference to the body element
+/* Toggle Style Switcher */
 
-    navLinks.classList.toggle('active'); // Toggle the nav-links visibility
-    body.classList.toggle('nav-active'); // Toggle the blur effect on body
-});
+const styleSwitcherToggle = document.querySelector('.style-switcher-toggler');
+styleSwitcherToggle.addEventListener('click', () => { document.querySelector('.style-switcher').classList.toggle('open'); })
+
+window.addEventListener('scroll', () => { if(document.querySelector('.style-switcher').classList.contains('open')) { document.querySelector('.style-switcher').classList.remove('open'); } })
+
+const alternateStyles = document.querySelectorAll('.alternate-style');
+function setActiveStyle(color) {
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute('title')) { style.removeAttribute('disabled'); } else { style.setAttribute('disabled', 'true'); }
+    })
+}
+
+/* Dark/Light Mode */
+
+const dayNight = document.querySelector('.day-night');
+dayNight.addEventListener('click', () => {
+    dayNight.querySelector('i').classList.toggle('fa-sun');
+    dayNight.querySelector('i').classList.toggle('fa-moon');
+    document.body.classList.toggle('dark');
+})
+
+window.addEventListener('load', () => {
+    if(document.body.classList.contains('dark')) { dayNight.querySelector('i').classList.add('fa-sun'); } else { dayNight.querySelector('i').classList.add('fa-moon'); }
+})
+
+/* Typing Animation */
+
+var typed = new Typed('.typing', { strings: ["", "AI Engineer", "Web Developer",""], typeSpeed: 100, Backspeed: 60, loop: true })
+
+/* Changing Aside Active Link */
+
+const nav = document.querySelector('.nav');
+const navList = nav.querySelectorAll('li');
+const totalNavList = navList.length;
+const allSection = document.querySelectorAll('.section');
+const totalSection = allSection.length;
+
+for(let i = 0; i < totalNavList; i++) {
+    const a = navList[i].querySelector('a');
+    a.addEventListener('click', function(){
+            removeBackSection();
+            for(let j = 0; j < totalNavList; j++) { 
+                if(navList[j].querySelector('a').classList.contains('active')) { addBackSection(j);/*allSection[j].classList.add('back-section');*/ }
+                navList[j].querySelector('a').classList.remove('active'); }
+        this.classList.add('active');
+        showSection(this);
+
+        if(window.innerWidth < 1200) { asideSectionTogglerBtn(); }
+    })
+}
+
+function addBackSection(num) { allSection[num].classList.add('back-section'); }
+
+function removeBackSection(){
+    for( let i = 0; i < totalSection; i++){ allSection[i].classList.remove('back-section'); }
+}
+
+function showSection(element){
+    for( let i = 0; i < totalSection; i++){ allSection[i].classList.remove('active'); }
+
+    const target = element.getAttribute("href").split("#")[1];
+    document.querySelector('#' + target).classList.add('active');
+}
+
+function updateNav(element){
+    for(let i = 0; i < totalNavList; i++){
+        navList[i].querySelector('a').classList.remove('active');
+        const target = element.getAttribute('href').split('#')[1];
+        if(target === navList[i].querySelector('a').getAttribute('href').split('#')[1]) { navList[i].querySelector('a').classList.add('active'); }
+        
+    }
+}
+
+
+
+/* Activating Mobile Menu */
+
+const navTogglerBtn = document.querySelector('.nav-toggler');
+const aside = document.querySelector('.aside');
+
+navTogglerBtn.addEventListener('click', () => { asideSectionTogglerBtn(); })
+
+function asideSectionTogglerBtn(){
+    aside.classList.toggle('open');
+    navTogglerBtn.classList.toggle('open');
+    for(let i = 0; i < totalSection; i++) { allSection[i].classList.toggle('open'); }
+}
+
+
